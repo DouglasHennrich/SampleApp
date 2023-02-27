4bcbbc88bc2ea9aeb5dbee426f567b86265c23b4
Douglas Hennrich <douglashennrich@yourdev.com.br>
2023-02-27 09:42:25 -0300

    Começando o arquivo de testes da MainViewModel

---
+++ SampleAppTests/Screens/Main/Presentation/ViewModel/MainViewModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewModelTests.swift
+//  SampleAppTests
+//
+//  Created by Douglas Hennrich on 25/02/23.
+//
+
+import XCTest
+@testable import SampleApp
+
+class MainViewModelTests: XCTestCase {
+  func test_should_get_weather_forecast() {
+
+  }
+}
